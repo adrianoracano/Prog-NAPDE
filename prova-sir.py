a538f20feb4f4f30c47b7ffe1b4247407375e751
AleSaccoExe <alessandrosacco002@gmail.com>
2023-05-26 11:47:21 +0200

    prova-sir.py reso più user friendly (da continuare)

--- prova-sir.py
+++ prova-sir.py
@@ -10,28 +10,63 @@ QUETA E' UNA PROVA PER IMPLEMENARE IL SIR
 """
 import tensorflow as tf
 import numpy as np
-from matplotlib import pyplot as plt
 import math
 import MyTemperatureGenerator as tg
-import MyCrankNicolsonClass as cnc
 import MyDatasetGenerator as dsg
 import HerRungeKutta as hrk
+import argparse
+import sys
+import pickle
+parser=argparse.ArgumentParser()
+parser.add_argument('-t', '--train', action = 'store_true', help = 'train the network')
+parser.add_argument('-ph','--print-help', action = 'store_true')
+parser.add_argument('-s', '--save-weights', help = 'save the weights in the specified file after the training', action = 'store_true')
+parser.add_argument('-fs', '--file-save', help = 'where to save the weigths. default: saved_weights.pkl', default = 'saved_weights.pkl')
+parser.add_argument('-l', '--load-weights', help = 'load the weights in the specified file before the training', action = 'store_true')
+parser.add_argument('-fl', '--file-load', help='where to save the weigths. default: saved_weights.pkl', default = 'saved_weights.pkl')
+parser.add_argument('-f', '--file', help = 'specify the file name. default: data.txt')
+parser.add_argument('-p', '--plot', help = 'number of plots after training', default = '0')
 
-learning_rate = 0.01
+args = parser.parse_args()
+
+if args.file_save:
+    print(args.file_save)
+    sys.exit()
+
+if args.print_help:
+    print('scrivere help per lo script')
+    sys.exit()
+
+data_dict = {}
+
+with open(args.file, 'r') as file:
+    for line in file:
+        line = line.strip()  # Remove leading/trailing whitespace
+        if line:
+            field, value = line.split(':')
+            data_dict[field.strip()] = value.strip()
+
+
+learning_rate = float(data_dict['learning_rate'])
 n_input = 2
-n_hidden = 32
+n_hidden = 10
 n_output = 1
 display_step = 10
 
 
-weights = {
-'h1': tf.Variable(tf.random.normal([n_input, n_hidden])),
-'out': tf.Variable(tf.random.normal([n_hidden, n_output]))
-}
-biases = {
-'b1': tf.Variable(tf.random.normal([n_hidden])),
-'out': tf.Variable(tf.random.normal([n_output]))
-}
+if args.load_weights:
+    with open('saved_variables.pkl', 'rb') as file:
+        weights, biases = pickle.load(args.file_load)
+
+if not args.load_weights:  
+    weights = {
+    'h1': tf.Variable(tf.random.normal([n_input, n_hidden])),
+    'out': tf.Variable(tf.random.normal([n_hidden, n_output]))
+    }
+    biases = {
+    'b1': tf.Variable(tf.random.normal([n_hidden])),
+    'out': tf.Variable(tf.random.normal([n_output]))
+    }
 
 
 # Stochastic gradient descent optimizer.
@@ -52,12 +87,12 @@ def g(y, v):
     x = tf.concat([y, tv], 1)
     return multilayer_perceptron(x)
 
-t_max = 1.0
-y0=0.05
-N = 60
+t_max = float(data_dict['t_max'])
+y0 = float(data_dict['beta0'])
+N = int(data_dict['N'])
 t = np.linspace(0, t_max, N)
 dt = t_max/N
-K=10 # numero di temperature
+K=int(data_dict['K']) # numero di temperature
 temperature= []
 I = np.zeros([K, N]) #da usare per gli infetti
 def f(beta, T):
@@ -75,14 +110,14 @@ data = {
 
 #save_dataset = dataset.copy()
 dataset = dsg.generate_dataset(temperature, data)
-a=3.0
+a=float(data_dict['alpha'])
 sir_0= np.array([10, 0.5, 0.])
 for k in range(K):
     s, i, r = hrk.RungeKutta(sir_0, dataset[1, k, ], N, t_max, a)
     I[k, ] = i.copy()
 dt = t_max/N
 #dataset = save_dataset
-step_summation = 1
+step_summation = int(data_dict['step_summation'])
 def custom_loss():
     total_summation = []
     for k in range(K):
@@ -113,11 +148,23 @@ def train_step():
     gradients = tape.gradient(loss, trainable_variables)
     optimizer.apply_gradients(zip(gradients, trainable_variables))
     
-training_steps = 500
-display_step = 50
+training_steps = int(data_dict['training_steps'])
+display_step = int(data_dict['display_step'])
 
-for i in range(training_steps):
-  train_step()
-  if i % display_step == 0:
-    print("iterazione %i:" % i)
-    print("loss: %f " % (custom_loss()))
+if args.train:
+    for i in range(training_steps):
+      train_step()
+      if i % display_step == 0:
+        print("iterazione %i:" % i)
+        print("loss: %f " % (custom_loss()))
+        
+
+if args.save_weights:
+    with open(args.file_save, 'wb') as file:
+        pickle.dump((weights, biases), args.file_save)
+
+n_plots = int(args.plot)
+for i in range(n_plots):
+    runfile('plot-sir.py')
+
+
