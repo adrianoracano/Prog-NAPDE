2a096e9a1f72f0d27617ea082d82b8c1afe98d47
Adriano Racano <10705130@polimi.it>
2023-12-16 12:23:21 +0100

    ora funziona

--- srcs/extract_infects.py
+++ srcs/extract_infects.py
@@ -10,60 +10,59 @@ pd.options.display.max_columns = 21
 
 def extract_infects(path, n_timesteps, start, n_mesi):
     dir_path = path
-    #ngiorni = len([entry for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))])
+    # ngiorni = len([entry for entry in os.listdir(dir_path) if os.path.isfile(os.path.join(dir_path, entry))])
     ngiorni = 366 * 2
 
     nreg = 21
     all_files = sorted(glob.glob(dir_path + "/*.csv"))
     df = pd.read_csv(all_files[0])
-    
+
     reg_list = list(df['denominazione_regione'])
     reg_list.remove('P.A. Trento')
     reg_list.remove('P.A. Bolzano')
-    reg_list.insert(len(reg_list),'P.A. Bolzano')
-    reg_list.insert(len(reg_list),'P.A. Trento')
+    reg_list.insert(len(reg_list), 'P.A. Bolzano')
+    reg_list.insert(len(reg_list), 'P.A. Trento')
     nomi_regioni = dict.fromkeys(reg_list, 0)
-    df=df.reindex(columns=reg_list)
+    df = df.reindex(columns=reg_list)
     tot_regione = {
-    	'Lombardia' :	9950742,
-    	'Lazio'	: 5707112,
-    	'Campania':	5592175,
-    	'Veneto':	4838253,
-      'Sicilia' :	4802016,
-      'Emilia-Romagna' :	4426929,
-    	'Piemonte' :	4240736,
-    	'Puglia' :	3900852,
-     	'Toscana' : 3651152,
-    	'Calabria' : 1841300,
-     	'Sardegna' :	1575028,
-    	'Liguria' :	1502624,
-    	'Marche' :	1480839,
-    	'Abruzzo' :	1269860,
-    	'Friuli Venezia Giulia'	: 1192191,
-    	'Umbria'	: 854137,
-    	'Basilicata'	: 536659,
-    	'Molise'	: 289840,
-    	"Valle d'Aosta"	: 122955,
-    	'P.A. Trento' : 542158,
-    	'P.A. Bolzano' : 532616}
+        'Lombardia': 9950742,
+        'Lazio': 5707112,
+        'Campania': 5592175,
+        'Veneto': 4838253,
+        'Sicilia': 4802016,
+        'Emilia-Romagna': 4426929,
+        'Piemonte': 4240736,
+        'Puglia': 3900852,
+        'Toscana': 3651152,
+        'Calabria': 1841300,
+        'Sardegna': 1575028,
+        'Liguria': 1502624,
+        'Marche': 1480839,
+        'Abruzzo': 1269860,
+        'Friuli Venezia Giulia': 1192191,
+        'Umbria': 854137,
+        'Basilicata': 536659,
+        'Molise': 289840,
+        "Valle d'Aosta": 122955,
+        'P.A. Trento': 542158,
+        'P.A. Bolzano': 532616}
     tot_regione = pd.Series(tot_regione)
-    
-    #dfi.loc[0] = pd.Series(np.array(df['totale_positivi']), index=dfi.columns)
-    
-    
-    #giorno = 1
-    dfi = pd.DataFrame(data = nomi_regioni, index = [0])
+
+    # dfi.loc[0] = pd.Series(np.array(df['totale_positivi']), index=dfi.columns)
+
+    # giorno = 1
+    dfi = pd.DataFrame(data=nomi_regioni, index=[0])
     giorno = 0
-    start_vec = [0]; #giorno da cui iniziare a registrare infetti
+    start_vec = [0];  # giorno da cui iniziare a registrare infetti
     for file in (all_files[start_vec[0]: start_vec[0] + ngiorni]):
         df = pd.read_csv(file)
         dfi.loc[giorno] = pd.Series(np.array(df['totale_positivi']), index=dfi.columns)
         # infetti[giorno][:] = (np.array(df['totale_positivi']))
         giorno = giorno + 1
-    
+
     dfi = dfi / tot_regione
     dfi = dfi.reindex(columns=reg_list)
-    dfi = dfi.drop(columns = ['Molise', "Valle d'Aosta"])
+    dfi = dfi.drop(columns=['Molise', "Valle d'Aosta"])
 
     n_giorni = math.floor(365 * n_mesi / 12)
     d, m, y = (int(s) for s in (re.findall(r'\b\d+\b', start)))
@@ -74,30 +73,27 @@ def extract_infects(path, n_timesteps, start, n_mesi):
     giorni_prec = giorni_prec + 1 * (y == 2020) + 366 * (y == 2021)
     index_start = -54 + giorni_prec + d;
 
-    dfi = dfi.loc[index_start : index_start + n_giorni - 1]
-    
+    dfi = dfi.loc[index_start: index_start + n_giorni - 1]
+
     I_vec = np.array(dfi).transpose()
 
-    #aggiunta Rt e beta con derivata logaritmica"
+    # Inizializzazione dell'array di output
+    new_indices = np.linspace(0, I_vec.shape[1] - 1, math.floor(50 * n_mesi / 12))
+    inf_interp = np.zeros(shape=(I_vec.shape[0], math.floor(50 * n_mesi / 12)))
+    spline_indices = np.linspace(0, I_vec.shape[1] - 1, n_timesteps)
+    inf_spline = np.zeros(shape=(I_vec.shape[0], n_timesteps))
+
+    # Interpolazione lineare lungo l'asse N per ogni riga
+    for i in range(I_vec.shape[0]):
+        inf_interp[i, :] = np.interp(new_indices, np.arange(I_vec.shape[1]), I_vec[i, :])
+        #       beta_interp[i, :] = np.interp(new_indices, np.arange(I_vec.shape[1]), I_vec[i, :])
+        spline = make_interp_spline(new_indices, inf_interp[i, :])
+        inf_spline[i, :] = spline(spline_indices)
+
+    # aggiunta Rt e beta con derivata logaritmica"
     alpha = 1.3
     mylog = np.vectorize(math.log)
-    R0_log = (mylog(I_vec[:,1]) - mylog(I_vec[:,0]))/alpha + 1
+    dt = n_mesi * 30 / n_timesteps
+    R0_log = (mylog(inf_spline[:, 1]) - mylog(inf_spline[:, 0])) / (dt * alpha) + 1
     beta0_log = R0_log * alpha;
-
-    # Inizializzazione dell'array di output
-    if n_timesteps < n_giorni:
-        new_indices = np.linspace(0, I_vec.shape[1] - 1, math.floor(50 * n_mesi / 365))
-        inf_interp = np.zeros((I_vec.shape[0], math.floor(50 * n_mesi / 365)))
-        inf_spline = np.zeros((I_vec.shape[0], n_timesteps))
-    #    beta_interp = np.zeros((I_vec.shape[0], n_timesteps))
-
-        # Interpolazione lineare lungo l'asse N per ogni riga
-        for i in range(I_vec.shape[0]):
-            inf_interp[i, :] = np.interp(new_indices, np.arange(I_vec.shape[1]), I_vec[i, :])
-    #       beta_interp[i, :] = np.interp(new_indices, np.arange(I_vec.shape[1]), I_vec[i, :])
-            spline = make_interp_spline(new_indices, inf_interp[:, i])
-            inf_spline[:, i] = spline(new_indices)
-
-    else:
-        inf_spline = I_vec
     return inf_spline, beta0_log
--- srcs/extract_zone.py
+++ srcs/extract_zone.py
@@ -274,15 +274,14 @@ def extract_zones(n_timesteps, start, n_mesi):
     dsz = dsz.loc[index[index_start : index_start + n_giorni]]
     
     Z_vec = np.array(dsz).transpose()
-    if n_timesteps < n_giorni:
-        arr_interp = np.zeros((Z_vec.shape[0], math.floor(50 * n_mesi / 365)))
-        new_indices = np.linspace(0, Z_vec.shape[1] - 1, n_timesteps)
-        zone_spline = np.zeros((Z_vec.shape[0], n_timesteps))
-        # Interpolazione lineare lungo l'asse N per ogni riga
-        for i in range(Z_vec.shape[0]):
-            arr_interp[i, :] = np.interp(new_indices, np.arange(Z_vec.shape[1]), Z_vec[i, :])
-            spline = make_interp_spline(new_indices, arr_interp[:, i])
-            zone_spline[:, i] = spline(new_indices)
-    else: zone_spline = Z_vec
+    arr_interp = np.zeros((Z_vec.shape[0], math.floor(50 * n_mesi / 12)))
+    new_indices = np.linspace(0, Z_vec.shape[1] - 1, math.floor(50 * n_mesi / 12))
+    spline_indices = np.linspace(0, Z_vec.shape[1] - 1, n_timesteps)
+    zone_spline = np.zeros((Z_vec.shape[0], n_timesteps))
+    # Interpolazione lineare lungo l'asse N per ogni riga
+    for i in range(Z_vec.shape[0]):
+        arr_interp[i, :] = np.interp(new_indices, np.arange(Z_vec.shape[1]), Z_vec[i, :])
+        spline = make_interp_spline(new_indices, arr_interp[i, :])
+        zone_spline[i, :] = spline(spline_indices)
     return zone_spline
 
