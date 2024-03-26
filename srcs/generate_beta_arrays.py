dce7ee9829d0067c54b7ea1d492eb9ba31cf0c6b
Adriano Racano <10705130@polimi.it>
2024-03-26 10:35:23 +0100

    un solo K

--- srcs/generate_beta_arrays.py
+++ srcs/generate_beta_arrays.py
@@ -9,7 +9,7 @@ from scipy.interpolate import make_interp_spline
 from utilities import SirHelperFunctions as shr
 import os
 import converti_ngiorni_data as cgd
-import matplotlib.animation as animation
+import random
 
 reg_list = ['Abruzzo',
      'Basilicata',
@@ -47,7 +47,7 @@ reg_list = ['Abruzzo',
 #regions = ['nome della regione'], in caso di più regioni torna un array (n_regioni * n_intervalli) * n_timesteps
 #dove le prime n_regioni colonne sono gli intervalli per quella regioni dalla data 1, le seconde dalla data 2 ecc..
 
-def generate_beta_arrays(path_i, n_giorni, max_months, overlap, start_date  = '15/03/2020', n_timesteps = None, regions = reg_list):
+def generate_beta_arrays(path_i, K, n_giorni, overlap, start_date  = '15/03/2020', n_timesteps = None, regions = reg_list):
     n_mesi = n_giorni / 30
     reg_list = ['Abruzzo',
      'Basilicata',
@@ -69,8 +69,8 @@ def generate_beta_arrays(path_i, n_giorni, max_months, overlap, start_date  = '1
      'P.A. Bolzano',
      'P.A. Trento']
     
-    
-    start = cgd.data_a_ngiorni(start_date)
+    #vecchia parte con max_months commentata
+    """ start = cgd.data_a_ngiorni(start_date)
     max_days = math.floor(max_months * 30)
     end = start + max_days
     end_date = cgd.ngiorni_a_data(end)
@@ -78,32 +78,53 @@ def generate_beta_arrays(path_i, n_giorni, max_months, overlap, start_date  = '1
     if end_y > 2021 or (end_y == 2021 and end_m>9) or (end_y == 2021 and end_m == 9 and end_d > 15):
         raise("Too many days")
     if overlap > n_giorni or n_giorni > max_days or overlap > max_days:
-        raise("Incompatible sizes")
+        raise("Incompatible sizes") """
     
+    
+    start = cgd.data_a_ngiorni(start_date)
+    end = start + (K) * (n_giorni - overlap) 
+    end_date = cgd.ngiorni_a_data(end)
+    if end > cgd.data_a_ngiorni("15/09/2021"):
+        raise ValueError('Too many intervals')
+   
+    #vecchia parte con max_months commentata
+    """ 
     #calcolo del numero di intervalli
     used_days = n_giorni
     count = 1
     while used_days <= max_days - n_giorni + overlap:
         used_days += n_giorni - overlap
         count += 1
-    print(str(count) + " intervals can be generated")
+    print(str(count) + " intervals can be generated") """
 
     if n_timesteps == None:
-        n_timesteps = n_giorni*2
+        n_timesteps = max(n_giorni*2, 100)
     
     infetti, beta_log = exi.extract_infects(path_i, n_timesteps, start_date, n_mesi, regions)
     rimossi =  exr.extract_removed(path_i, n_timesteps, start_date, n_mesi, regions)
     start_new = cgd.ngiorni_a_data(cgd.data_a_ngiorni(start_date) + n_giorni - overlap)
 
+    #vecchia parte con max_months commentata
+    """ 
     for i in range(count - 1):
         start_new = cgd.ngiorni_a_data(cgd.data_a_ngiorni(start_new) + n_giorni - overlap)
         infetti_new, beta_log_new = exi.extract_infects(path_i, n_timesteps, start_new, n_mesi, regions)
         infetti = np.concatenate((infetti, infetti_new), axis = 0)
         beta_log = np.concatenate((beta_log, beta_log_new), axis = 0)
         rimossi_new =  exr.extract_removed(path_i, n_timesteps, start_new, n_mesi, regions)
-        rimossi = np.concatenate((rimossi, rimossi_new), axis = 0)
+        rimossi = np.concatenate((rimossi, rimossi_new), axis = 0) """
 
+    
+    for i in range(K - 1):
+        start_new = cgd.ngiorni_a_data(cgd.data_a_ngiorni(start_new) + n_giorni - overlap)
+        infetti_new, beta_log_new = exi.extract_infects(path_i, n_timesteps, start_new, n_mesi, regions)
+        infetti = np.concatenate((infetti, infetti_new), axis = 0)
+        beta_log = np.concatenate((beta_log, beta_log_new), axis = 0)
+        rimossi_new =  exr.extract_removed(path_i, n_timesteps, start_new, n_mesi, regions)
+        rimossi = np.concatenate((rimossi, rimossi_new), axis = 0)
+      
     #smoothing infetti ===============================================
+    """ 
     smooth = False
     if smooth :
         # Inizializzazione dell'array di output
@@ -114,7 +135,7 @@ def generate_beta_arrays(path_i, n_giorni, max_months, overlap, start_date  = '1
         inf_interp = np.zeros(shape=(I_vec.shape[0], n_interp_point))
         spline_indices = np.linspace(0, I_vec.shape[1] - 1, n_timesteps + 1)
         inf_spline = np.zeros(shape=(I_vec.shape[0], n_timesteps + 1))
-
+ """
     #beta sintetico ==================================================
     use_positive = True #per tagliare beta_log negativo
     if use_positive:
@@ -123,7 +144,7 @@ def generate_beta_arrays(path_i, n_giorni, max_months, overlap, start_date  = '1
     use_up_bound = True
     if use_up_bound:
         beta_log = beta_log.clip(max = up_bound)
-    K_b = 1 / 20 # percentule di timestep per interpolare beta, regola lo smoothing di beta
+    K_b = 1 / 3 # percentule di timestep per interpolare beta, regola lo smoothing di beta
     n_interp_points = math.floor((n_timesteps) * K_b)
     new_indices = np.linspace(0, beta_log.shape[1], n_interp_points)
     beta_log_interp = np.zeros(shape=(beta_log.shape[0], n_interp_points))
@@ -178,7 +199,21 @@ def generate_beta_arrays(path_i, n_giorni, max_months, overlap, start_date  = '1
     alpha = 1.3
     infetti_SIR = shr.compute_I([beta_spline], n_mesi, alpha, sir0)
     infetti_SIR = infetti_SIR[0]
-    return beta_spline, infetti_SIR
+
+    """ 
+    #select K_val samples randomly
+    infetti_SIR_val = np.zeros(shape = (K_val, n_timesteps))
+    beta_spline_val = np.zeros(shape = (K_val, n_timesteps))
+    for i in range(K_val):
+        random_id = random.randint(0, (infetti_SIR.shape)[0] - 1)
+        infetti_SIR_val[i, :] = infetti_SIR_val[random_id, :]
+        infetti_SIR = np.delete(infetti_SIR, random_id, axis = 0)
+        print(infetti_SIR.shape)
+        beta_spline_val[i, :] = beta_spline_val[random_id, :]
+        beta_spline = np.delete(beta_spline, random_id, axis = 0)
+      """
+
+    return beta_spline, infetti_SIR #, beta_spline_val, infetti_SIR_val
 
 
 
