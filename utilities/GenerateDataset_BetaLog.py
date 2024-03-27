6f087acaf263bb4c39460ee877f63d860166ef91
AleSaccoExe <alessandrosacco002@gmail.com>
2024-03-27 11:41:20 +0100

    corretto un errore in GenerateDataset_BetaLog

--- utilities/GenerateDataset_BetaLog.py
+++ utilities/GenerateDataset_BetaLog.py
@@ -16,14 +16,14 @@ K_train = 15 # il resto è messo nel validation set
 beta, infetti = gba.generate_beta_arrays(path_i, K, n_giorni, overlap)
 temp = gta.generate_temp_arrays(path_t, n_giorni, max_months, overlap)
 
-temp_train = np.zeros((K_train, n_timesteps))
-beta_train = np.zeros((K_train, n_timesteps))
-infetti_train = np.zeros((K_train, n_timesteps))
+temp_train = temp[0:K_train, :]
+beta_train = beta[0:K_train, :]
+infetti_train = infetti[0:K_train, :]
 
 K_val = temp.shape[0] - K_train
-temp_val = np.zeros((K_val, n_timesteps))
-beta_val= np.zeros((K_val, n_timesteps))
-infetti_val = np.zeros((K_val, n_timesteps))
+temp_val = temp[K_train+1:, :]
+beta_val = beta[K_train+1:, :]
+infetti_val = infetti[K_train+1:, :]
 
 with open('datasets/'+nome_file, 'wb') as file:
     pickle.dump((infetti_train, infetti_val, temp_train, temp_val, beta_train, beta_val), file)
